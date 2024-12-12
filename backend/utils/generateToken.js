fd4b08cd7ee007a2e62dc4d69a8c09928bd1c51a
User <mahmudovarahide@gmail.com>
2024-12-12 13:40:56 +0400

    test

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import { ENV_VARS } from "../config/envVars.js";
 
 export const generateTokenAndSetCookie = (userId, res) => {
-	const token = jwt.sign({ userId }, ENV_VARS.JWT_SECRET, { expiresIn: "15d" });
+	const token = jwt.sign({ userId }, ENV_VARS.JWT_SECRET, { expiresIn: "1h" });
 
 	res.cookie("jwt-netflix", token, {
 		maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in MS
