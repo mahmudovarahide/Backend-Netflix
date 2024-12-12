df7b69b34526478214fdb94e17f106e4381ff807
User <mahmudovarahide@gmail.com>
2024-12-12 14:22:11 +0400

    idk

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,6 +5,7 @@ export const connectDB = async () => {
 	try {
 		const conn = await mongoose.connect(ENV_VARS.MONGO_URI);
 		console.log("MongoDB connected: " + conn.connection.host);
+		console.log("MONGO_URI Debug:", process.env.MONGO_URI); // Debug MONGO_URI
 	} catch (error) {
 		console.error("Error connecting to MONGODB: " + error.message);
 		process.exit(1);
