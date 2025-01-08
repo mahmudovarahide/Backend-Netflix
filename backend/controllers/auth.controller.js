b6326660f81acf300cab8f79f631824b09ff194f
User <mahmudovarahide@gmail.com>
2025-01-08 21:12:12 +0400

    bachk

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -54,7 +54,7 @@ export async function signup(req, res) {
 		});
 	} catch (error) {
 		console.log("Error in signup controller", error.message);
-		res.status(500).json({ success: false, message: "Internal server error" });
+		res.status(500).json({ success: false, message: `Internal serverrr error ${error} ` });
 	}
 }
 
@@ -88,7 +88,7 @@ export async function login(req, res) {
 		});
 	} catch (error) {
 		console.log("Error in login controller", error.message);
-		res.status(500).json({ success: false, message: "Internal server error" });
+		res.status(500).json({ success: false, message: `Internal serverrr error ${error} ` });
 	}
 }
 
@@ -98,7 +98,7 @@ export async function logout(req, res) {
 		res.status(200).json({ success: true, message: "Logged out successfully" });
 	} catch (error) {
 		console.log("Error in logout controller", error.message);
-		res.status(500).json({ success: false, message: "Internal server error" });
+		res.status(500).json({ success: false, message: `Internal serverrr error ${error} ` });
 	}
 }
 
@@ -108,6 +108,6 @@ export async function authCheck(req, res) {
 		res.status(200).json({ success: true, user: req.user });
 	} catch (error) {
 		console.log("Error in authCheck controller", error.message);
-		res.status(500).json({ success: false, message: "Internal server error" });
+		res.status(500).json({ success: false, message: `Internal serverrr error ${error} ` });
 	}
 }
