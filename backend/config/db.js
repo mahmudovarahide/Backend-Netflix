8f91199292e7f17946e8bcdaf9b340bbf3b85a92
User <mahmudovarahide@gmail.com>
2025-01-14 14:39:52 +0400

    new header1

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,10 @@
 import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(`Error: ${err.message}`);
-    process.exit(1); // Stop the server if DB connection fails
+    process.exit(1);
   }
 };
